ef44888dc48d01c40fc2038c19c38da5f212a274
Bhavneet singh dhawan <bhavneetsinghdhawan@gmail.com>
2026-08-07 23:23:07 +0530

    Time: 15 ms (27.85%), Space: 12.8 MB (7.84%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        n = len(s)
+
+        for i in range(1, n):
+            if n % i == 0:
+                part = s[:i]
+
+                if part * (n // i) == s:
+                    return True
+
+        return False
